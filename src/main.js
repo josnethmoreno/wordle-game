682be2c3d4b71629c7be58f0cbb96da7f5f49e89
josnethmoreno <josnethmoreno@gmail.com>
2022-07-06 11:48:17 -0400

    regex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 import 'remixicon/fonts/remixicon.css'
 
+const tiles = document.querySelectorAll('.game-tile');
+const rows = document.querySelectorAll('.game-row');
+
+const letterRegex = /[a-z]/i;
+let tilePosition = 0;
+
 document.addEventListener('keydown', (e) => {
-	console.log(e.key)
+	if(e.key.match(letterRegex) && e.key.length === 1) {
+		console.log(tiles[tilePosition])
+	}
 })
