11eb7cb9e118025f0ba495305660c47ed4ada4da
josnethmoreno <josnethmoreno@gmail.com>
2022-07-11 18:04:09 -0400

    guess word

--- src/main.js
+++ src/main.js
@@ -1,50 +1,90 @@
 import './style.css'
-import 'remixicon/fonts/remixicon.css'
 
-const tiles = document.querySelectorAll('.game-tile');
-let tilePosition = 0;
-const rows = document.querySelectorAll('.game-row');
-let rowPosition = 0;
-const word = 'Julia'
-const letterRegex = /[a-z]/i;
+import 'remixicon/fonts/remixicon.css'
+import 'animate.css';
+
+import { words, word } from '/src/words.js'
+
+const tiles = document.querySelectorAll('.game-tile')
+let positionTile = 0
+const rows = document.querySelectorAll('.game-row')
+let positionRow = 0
+let userWord = ''
+const letterRegex = /[a-z]/i
 
 document.addEventListener('keydown', (e) => {
 	if(e.key.match(letterRegex) && e.key.length === 1) {
-		updateLetter(e.key)
+		typeLetter(e.key.toLowerCase());
+	}
+
+	if(e.key.match(letterRegex) && e.key === 'Enter') {
+		guessWord(userWord)
+	}
+
+	if(e.key.match(letterRegex) && e.key === 'Backspace') {
+		deleteLetter()
 	}
 })
 
-let wordUser = '';
-
-const updateLetter = (letter) => {
-	tiles[tilePosition].setAttribute('data-letter', letter)
-	tilePosition++
-	wordUser += letter
-
-	if(tilePosition === 5 || tilePosition === 10 || tilePosition === 15 || tilePosition === 20 || tilePosition === 25 || tilePosition === 30 ) {
-		guessWord(wordUser)
-		rowPosition++
-	}
-
-	if(tilePosition === 30) {
-		endGame()
+const typeLetter = (key) => {
+	if (userWord.length < 5) {
+		rows[positionRow].children[positionTile].setAttribute('data-letter', key)
+		positionTile++
+		userWord += key;
 	}
 }
 
-const guessWord = (word) => {
-	rows[rowPosition].setAttribute('data-word', wordUser)
-	wordUser = ''
+const deleteLetter = () => {
+	if(userWord.length === 0) return 
+
+	positionTile--
+	userWord = userWord.slice(0, -1)
+	rows[positionRow].children[positionTile].removeAttribute('data-letter')
 }
 
-const endGame = () => {
-	tilePosition = 0
-	rowPosition = 0
+const guessWord = (userWord) => {
+	if (userWord.length < 5) return console.error('No engouh letter')
+	if (!words.includes(userWord)) return console.error('No word in list')
+
+	const arrayWord = word.split('');
+	arrayWord.forEach((letter, index) => {
+		const dataLetter = rows[positionRow].children[index].getAttribute('data-letter');
+		if (arrayWord.includes(dataLetter)) {
+			if (letter === dataLetter) {
+				return rows[positionRow].children[index].setAttribute('data-status', 'true')
+			}
+			return rows[positionRow].children[index].setAttribute('data-status', 'find')
+		}
+		rows[positionRow].children[index].setAttribute('data-status', 'false')
+	})
+
+	if(userWord === word) {
+		return winGame()
+	}
+
+	updateRow()
+}
+
+const updateRow = () => {
+	positionRow += 1
+	positionTile = 0
+	userWord = ''
+}
+
+const winGame = () => {
+	modalStats();
+}
+
+const resetGame = () => {
+	positionRow = 0
+	positionTile = 0
+	userWord = ''
+
 	tiles.forEach((tile) => {
-		tile.setAttribute('data-letter', '')
-	})
-	rows.forEach((row) => {
-		row.setAttribute('data-word', '')
+		tile.removeAttribute('data-letter')
+		tile.removeAttribute('data-status')
 	})
 }
+
 
 
---
+++ src/words.js
@@ -0,0 +1,14 @@
+let word = 'beach'
+
+let words = [
+	'abuse',
+	'adult',
+	'agent',
+	'anger',
+	'apple',
+	'award',
+	'basis',
+	'beach'
+]
+
+export { word, words }
